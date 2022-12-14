dfa8bae8efe34bc1b57bd5be780a21406532d33b
matheus.dias <matheus.dias@GPK597>
2022-12-14 16:50:41 -0300

    Implementação da classe Model Ajuste na Program

---
+++ AutenticacaoGoogleTeste/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AutenticacaoGoogleTeste.Models
+{
+    [Table("User")]
+    public class UserModel
+    {
+        public int UserId { get; set; }
+
+        [Display(Name = "Email")]
+        public string UserEmail { get; set; }
+
+        public bool verifiedUser { get; set; }
+
+        [Display(Name = "Nome do Usuário")]
+        public string userName { get; set; }
+    }
+}
